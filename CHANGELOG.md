4103633147c9811bd1853a2ec784e862809f6adf
bloom-lmh <1357526355@qq.com>
2025-01-14 21:47:00 +0800

    对配置进行了规定

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,38 +1,48 @@
 # 更新日志 / Change Log
 
 ## [1.0.3] - 2024-12-26
-### 添加
-- 添加了基于抽象语法树的解析机制，提高了成员识别能力，但基于抽象语法树的生成性能较差.
+### 添加/add
+- 添加了基于抽象语法树的解析机制，提高了成员识别能力，但基于抽象语法树的生成性能较差。
 - Introduced an abstract syntax tree-based parsing mechanism, improving member recognition ability, although the performance of AST generation is not optimal.
 
-### 更改
-- 优化了正则表达式，使其能够正确匹配多行方法体并提取其中的异常类型.
+### 更改/update
+- 优化了正则表达式，使其能够正确匹配多行方法体并提取其中的异常类型。
 - Optimized the regular expression to correctly match multi-line method bodies and extract the exception types within them.
 
-### 修复
-- 修复了 `parseMethod` 正则无法匹配方法体中 `throw` 异常的问题.
+### 修复/fix
+- 修复了 `parseMethod` 正则无法匹配方法体中 `throw` 异常的问题。
 - Fixed the issue where the `parseMethod` regular expression could not match `throw` exceptions in method bodies.
 
 ## [1.0.4] - 2024-12-27
-### 更改
-- 更新了 Markdown 中英文文档的链接.
+### 更改/update
+- 更新了 Markdown 中英文文档的链接。
 - Updated the English documentation links in the Markdown files.
 
 
 ## [1.0.5] - 2024-1-09
-### 修复
-- 修复了没有项目路径的文件不可生成注释的问题.
+### 修复/fix
+- 修复了没有项目路径的文件不可生成注释的问题。
 - Fixed an issue where files without project paths could not generate comments.
 
-### 更改
+### 更改/update
 - 优化代码结构，引入职责链模式处理注释成员生成。
 - Optimize code structure and introduce chain of responsibility mode to handle comment member generation.
 
+## [1.0.6] - 2024-1-09 (开发中......)
+### 修复/fix
+- 采用默认注解进行填充的方法修复了对未知复杂代码结构不可生成注解的问题，提高了代码的健壮性，但这只是一种简单处理的方式，后续会尽量包括更多的代码结构支持。
+- Filling with default annotations fixes the problem of not generating annotations for unknown complex code structures and improves the robustness of the code, but this is only a simple way to deal with it and will include more code structure support in the future.
+### 更改/update
+- 对全文件注解代码进行优化，全文件注解生成速度较上一版本有所提升。
+- The full-file annotation code is optimized, and the generation speed of full-file annotation is improved compared with the previous version.
+
 
 后续开发计划
-- 解决泛型的问题，消除Interface extends bug
-- 提高弹性和健壮性允许错误的发生而不影响大部分行为的执行
-- 优化多个注释生成代码
+- 添加对泛型的支持，消除Interface extends bug
+- 配置方式多元化 vscode配置（使用文件的方式链接到文件） < json配置 (json配置生成器来生成、也允许用户手动去添加)
+- *提高弹性和健壮性允许错误的发生而不影响大部分行为的执行（只是简单的处理了）
+- *优化多个注释生成代码
+- 解决生成注释的格式问题
 - 多元化配置方式以及配置项目的确定
 - 加入翻译功能以及单词映射
 - 加入get set 方法
---
+++ src/config/configType.ts
@@ -0,0 +1,70 @@
+interface BaseAnnotationConfig {
+  isAuthorTag: boolean;  // 是否包含作者信息
+  isAccessTag: boolean;  // 是否包含访问控制
+  isAliasTag: boolean;  // 是否包含别名
+  isVersionTag: boolean;  // 是否包含版本
+  isNameTag: boolean;  // 是否包含名称
+  isDescriptionTag: boolean;  // 是否包含描述
+  isLicenseTag: boolean;  // 是否包含许可证
+  isCopyrightTag: boolean;  // 是否包含版权
+  isSeeTag: boolean;  // 是否包含 See 参考
+  isSummaryTag: boolean;  // 是否包含摘要
+  isExampleTag: boolean;  // 是否包含示例
+  isTemplateTag: boolean;
+  isNamespaceTag: boolean;// 声明模块名称和命名空间，组织代码结构。
+  isModuleTag: boolean;  // 声明模块名称和命名空间，组织代码结构。
+  isRequireTag: boolean;  // 指明文件的依赖模块。
+  isFileoverview: boolean
+}
+// 文件级别的标签
+type onlyFile = 'isNamespaceTag' | 'isModuleTag' | 'isRequireTag' | 'isFileoverview' | 'isLicenseTag' | 'isCopyrightTag' | 'isSummaryTag'
+
+interface ClassAnnotationConfig extends Omit<BaseAnnotationConfig, 'isAccessTag' | onlyFile> {
+  isClassTag: boolean;  // 是否是类
+  isAbstractTag: boolean;  // 是否是抽象类
+  isExtendsTag: boolean;  // 是否继承
+  isImplementsTag: boolean;  // 是否实现接口
+}
+interface MethodAnnotationConfig extends Omit<BaseAnnotationConfig, onlyFile> {
+  isAsyncTag: boolean;  // 是否异步
+  isFunctionTag: boolean;  // 是否是函数
+  isConstructorTag: boolean;  // 是否是构造函数
+  isThrowsTag: boolean;  // 是否包含抛出异常
+  isParamsTag: boolean;  // 是否包含参数
+  isReturnsTag: boolean;  // 是否包含返回值
+  isStaticTag: boolean;  // 是否是静态方法
+}
+interface PropertyAnnotationConfig extends Omit<BaseAnnotationConfig, 'isAuthorTag' | 'isAccessTag' | 'isVersionTag' | onlyFile> {
+  isPropertyTag: boolean;  // 是否是属性
+  isStaticTag: boolean;  // 是否是静态属性
+  isTypeTag: boolean;  // 是否包含类型
+  isDefaultTag: boolean;  // 是否包含默认值
+}
+interface InterfaceAnnotationConfig extends Omit<BaseAnnotationConfig, 'isAccessTag' | onlyFile> {
+  isInterfaceTag: boolean;  // 是否是接口
+  isExtendsTag: boolean;  // 是否继承
+}
+interface EnumAnnotationConfig extends Omit<BaseAnnotationConfig, 'isAuthorTag' | 'isAccessTag' | 'isVersionTag' | onlyFile> {
+  isEnumTag: boolean;  // 是否是枚举类型
+}
+interface TypedefAnnotationConfig extends Omit<BaseAnnotationConfig, 'isAuthorTag' | 'isAccessTag' | 'isVersionTag' | onlyFile> {
+  isTypedefTag: boolean;  // 是否是自定义类型
+  isTypeTag: boolean;  // 是否是类型
+}
+interface FileAnnotationConfig extends Omit<BaseAnnotationConfig, 'isAccessTag' | 'isAliasTag' | 'isVersionTag' | 'isNameTag' | 'isDescription' | 'isSummaryTag' | 'isExampleTag'> {
+
+}
+interface TranslateConfig {
+  isOpen: boolean;
+  api: Map<string, Array<string>> | Array<string> | string
+  wordMap: { [key: string]: string };
+  isMemoryEnabled: boolean;
+}
+interface SystemConfig {
+  // 注解生成策略 混合策略 正则策略 抽象语法树策略 默认采用混合策略，抽象语法树策略性能有缺陷
+  annotationCreateStrategy: number
+  // 注解生成方式 生成时删除原来的注解 生成时追加注解 生成时更新已有注解
+  annotationCreateMode: number
+  // 开启配置缓存
+  configCache: boolean
+}
